747ab2c57f4b7c9cd2d6b56b42d57720d0079c3e
Megan Penn <penn.meg@gmail.com>
2018-12-06 11:37:28 -0500

    AddContact

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -5,8 +5,12 @@ class AddContact extends React.Component {
     return (
       <form className="contact-form">
         <h2>Add Contact Information</h2>
-        <input type="text" />
-        <button type="submit"></button>
+        <input name="firstName" type="text" placeholder="First Name"/>
+        <input name="lastName" type="text" placeholder="Last Name"/>
+        <input name="emailAddress" type="text" placeholder="Email Address"/>
+        <input name="phoneNumber" type="number" placeholder="Phone Number"/>
+        <input name="companyName" type="text" placeholder="Company"/>
+        <button type="submit">+ Add Contact</button>
       </form>
     );
   }
